a821ed02408d13289656f5bcb6b067d178588f17
Nino Di Natale <nino@dinatale.io>
2023-04-18 21:26:20 +0200

    change animated int animation

--- lib/widgets/animated_int_widget.dart
+++ lib/widgets/animated_int_widget.dart
@@ -4,7 +4,8 @@ class AnimatedInt extends StatefulWidget {
   final int value;
   final TextStyle? textStyle;
 
-  const AnimatedInt({Key? key, required this.value, this.textStyle}) : super(key: key);
+  const AnimatedInt({Key? key, required this.value, this.textStyle})
+      : super(key: key);
 
   @override
   _AnimatedIntState createState() => _AnimatedIntState();
@@ -25,28 +26,34 @@ class _AnimatedIntState extends State<AnimatedInt> {
       duration: const Duration(milliseconds: 300),
       transitionBuilder: (Widget child, Animation<double> animation) {
         final position = Tween<Offset>(
-            begin: (_previous < widget.value)
-                ? (animation.status == AnimationStatus.completed)
-                ? const Offset(0, 1)
-                : const Offset(0, -1)
-                : (animation.status == AnimationStatus.completed)
-                ? const Offset(0, -1)
-                : const Offset(0, 1),
-            end: Offset.zero)
+                begin: (_previous < widget.value)
+                    ? (animation.status == AnimationStatus.completed)
+                        ? const Offset(0, 1)
+                        : const Offset(0, -1)
+                    : (animation.status == AnimationStatus.completed)
+                        ? const Offset(0, -1)
+                        : const Offset(0, 1),
+                end: Offset.zero)
             .animate(animation);
-        return FadeTransition(
-          opacity: animation,
-          child: SlideTransition(
-            position: position,
-            child: child,
-          ),
-        );
+
+        final scale = CurveTween(
+                curve: (_previous < widget.value)
+                    ? (animation.status == AnimationStatus.completed)
+                        ? Curves.easeInOutBack
+                        : Curves.easeInOutBack.flipped
+                    : (animation.status == AnimationStatus.completed)
+                        ? Curves.easeInOutBack.flipped
+                        : Curves.easeInOutBack)
+            .animate(animation);
+        return ScaleTransition(
+            scale: scale,
+            child: SlideTransition(
+              position: position,
+              child: child,
+            ));
       },
       child: Text(
         '${widget.value}',
-        // This key causes the AnimatedSwitcher to interpret this as a "new"
-        // child each time the count changes, so that it will begin its animation
-        // when the count changes.
         key: ValueKey<int>(widget.value),
         style: widget.textStyle,
       ),
